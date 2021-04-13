f648a1565e7fee399192ca86d60723eeaf700e5c
Leon <leon@famnabuurs.nl>
2021-04-13 10:02:17 +0200

    opdracht afgerond

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const schilderdemuur = function(richting ="unknown",kleur = "red") {
+    console.log("The "+ richting +" wall is painted " + kleur);
+}
+
+schilderdemuur();
+schilderdemuur("","green");
+schilderdemuur("north","orange");
+schilderdemuur("south","grey");
